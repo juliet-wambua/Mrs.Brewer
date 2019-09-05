e1b3d38b8a38840e45747fce6cd3432d5b802256
juliet-wambua <julienea289@gmail.com>
2019-09-05 14:52:47 +0300

    Created a Brewer Activity

---
+++ app/src/main/java/com/nea/mrbrewer/BrewerActivity.java
@@ -0,0 +1,14 @@
+package com.nea.mrbrewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_brewer );
+    }
+}
--- app/src/main/java/com/nea/mrbrewer/MainActivity.java
+++ app/src/main/java/com/nea/mrbrewer/MainActivity.java
@@ -2,25 +2,26 @@ package com.nea.mrbrewer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button mFindBrewersButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_main );
-
-        Button mFindBreweriesButton = (Button) findViewById ( R.id.findRestaurantsButton );
-        mFindBreweriesButton
-                .setOnClickListener ( new View.OnClickListener () {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
-                    }
-                });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFindBrewersButton = (Button) findViewById(R.id.findBrewersButton);
+        mFindBrewersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BrewerActivity
+                        .class);
+                startActivity(intent);
+            }
+        });
     }
 }
