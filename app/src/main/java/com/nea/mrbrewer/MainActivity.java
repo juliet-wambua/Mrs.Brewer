d94af39e80c2b38aed9419dcb586a8f5fef9791b
juliet-wambua <julienea289@gmail.com>
2019-09-05 14:59:27 +0300

    added methods to gather data and passit to another Activity

--- app/src/main/java/com/nea/mrbrewer/MainActivity.java
+++ app/src/main/java/com/nea/mrbrewer/MainActivity.java
@@ -4,22 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 private Button mFindBrewersButton;
+    private EditText mLocationEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindBrewersButton = (Button) findViewById(R.id.findBrewersButton);
         mFindBrewersButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, BrewerActivity
-                        .class);
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+                Intent intent = new Intent(MainActivity.this, BrewerActivity.class);
                 startActivity(intent);
             }
         });
