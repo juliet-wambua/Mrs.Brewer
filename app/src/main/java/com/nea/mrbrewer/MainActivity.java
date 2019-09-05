1ea8f3bd7b24ad75602931a2ea7845aad5cf2391
juliet-wambua <julienea289@gmail.com>
2019-09-05 17:21:34 +0300

    changed the view list to display items above the layout

--- app/src/main/java/com/nea/mrbrewer/MainActivity.java
+++ app/src/main/java/com/nea/mrbrewer/MainActivity.java
@@ -3,40 +3,48 @@ package com.nea.mrbrewer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.findBrewersButton) Button mfindBrewersButton;
-    @BindView(R.id.locationEditText) EditText mLocationEditText;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.findBrewersButton)
+    Button mfindBrewersButton;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
     @BindView(R.id.appNameTextView)
     TextView mAppNameTextView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
+        ButterKnife.bind ( this );
 
 
-        mfindBrewersButton.setOnClickListener(new View.OnClickListener() {
+        mfindBrewersButton.setOnClickListener ( new View.OnClickListener () {
+
             @Override
             public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, BrewerActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
+                if (v == mfindBrewersButton) {
+                    String location = mLocationEditText.getText ().toString ();
+                    Intent intent = new Intent ( MainActivity.this, BrewerActivity.class );
+                    intent.putExtra ( "location", location );
+                    startActivity ( intent );
+                }
             }
-        });
+        } );
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
+
