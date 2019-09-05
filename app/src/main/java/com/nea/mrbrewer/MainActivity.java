26453140f6d6228cddeaa32df7942e7e0af88c7d
juliet-wambua <julienea289@gmail.com>
2019-09-05 14:39:06 +0300

    added toasts to mr.brewer

--- app/src/main/java/com/nea/mrbrewer/MainActivity.java
+++ app/src/main/java/com/nea/mrbrewer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
                 .setOnClickListener ( new View.OnClickListener () {
                     @Override
                     public void onClick(View v) {
-                        //do something
+                        Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
                     }
-                } );
+                });
     }
 }
