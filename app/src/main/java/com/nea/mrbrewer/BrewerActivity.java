b59deda7cfce82ba8e3d36e390546d78261b6e48
juliet-wambua <julienea289@gmail.com>
2019-09-05 15:33:13 +0300

    added a string of beer

--- app/src/main/java/com/nea/mrbrewer/BrewerActivity.java
+++ app/src/main/java/com/nea/mrbrewer/BrewerActivity.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class BrewerActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] beers = new String[] {"Tusker", "Balozi","Pilsner","Stout","Porter","Bock","Weissbie","Lambic"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brewer);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, beers);
+        mListView.setAdapter(adapter);
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
+        mLocationTextView.setText("Here are all the beers : " + location);
+
+
     }
 }
