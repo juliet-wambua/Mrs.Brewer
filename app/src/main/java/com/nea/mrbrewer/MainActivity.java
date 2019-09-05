d1e3c83d1502d9e64512103e34ac4d04850ec932
juliet-wambua <julienea289@gmail.com>
2019-09-05 14:38:20 +0300

    added buttons.

--- app/src/main/java/com/nea/mrbrewer/MainActivity.java
+++ app/src/main/java/com/nea/mrbrewer/MainActivity.java
@@ -3,6 +3,8 @@ package com.nea.mrbrewer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        Button mFindBreweriesButton = (Button) findViewById ( R.id.findRestaurantsButton );
+        mFindBreweriesButton
+                .setOnClickListener ( new View.OnClickListener () {
+                    @Override
+                    public void onClick(View v) {
+                        //do something
+                    }
+                } );
     }
 }
